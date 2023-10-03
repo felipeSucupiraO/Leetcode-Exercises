6e66c0b096417e458d121d30a56b2ccedf85a92e
Aust2000 <58998427+Aust2000@users.noreply.github.com>
2023-10-03 15:28:09 -0300

    Update rotate_image.js

--- rotate_image.js
+++ rotate_image.js
@@ -4,19 +4,36 @@
 
 function rotateMatrix(matrix) {
     let n = matrix.length
-    let result = [n][n];
     let element;
 
-    for (let i = 0; i <= n; i++) {
-        for (let j = 0; j <= n; j++) {
+    var result = createMatrix(n, n);
+
+    for (let i = 0; i < n; i++) {
+        var clm = (n - i - 1);
+        for (let j = 0; j < n; j++) {
             element = matrix[i][j];
-            result[j][n - i + 1] = element;            
+            result[j][clm] = element;            
         }
     }
 
     matrix = result;
     return matrix;
 };
+
+function createMatrix(lns, clms) {
+    var matrix = [];
+    var sub = [];
+    
+    for (let j = 0; j < clms; j++) {
+        sub.push(0);
+    }
+
+    for (let i = 0; i < lns; i++) {
+        matrix[i] = sub;
+    }
+
+    return matrix;
+}
 
 function printMatrix(matrix) {
     for (let i = 0; i <= matrix.length; i++) {
