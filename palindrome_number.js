d189805a554b944b09dc4e8fc4ed05d5016310df
Aust2000 <felipeuberaba10@gmail.com>
2023-11-07 12:09:56 -0300

    partial palindrome number

---
+++ palindrome_number.js
@@ -0,0 +1,14 @@
+/* 
+Given an integer x, return true if x is a palindrome, and false otherwise.
+*/
+
+function isPalindrome(x) {
+    if (x < 0)
+        return false;
+
+    var size = Math.floor(Math.log10(x) + 1);
+    var i = 0;
+    while (true) {
+        if (x )
+    }
+}
